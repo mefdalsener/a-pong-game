e166eed30d8a9596981e95919a6ff04410edc498
Muhammed Efdal Şener <m.efdal66@hotmail.com>
2026-06-16 18:33:07 +0300

    random ball direction

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine.UIElements;
 
 public class BallMovement : MonoBehaviour
 {
+
     [SerializeField] float speed = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        int randomDirection = UnityEngine.Random.Range(0, 2) == 0 ? -1 : 1;
+
         Rigidbody rb = GetComponent<Rigidbody>();
-        rb.AddForce(Vector3.left * speed, ForceMode.VelocityChange);
+        rb.AddForce(Vector3.right * speed * randomDirection, ForceMode.VelocityChange);
     }
 
     // Update is called once per frame
